88b62a6f5fedf52273a3d06324ae0d2a31df4fb4
Glen Wilson <12glenwilson@gmail.com>
2016-10-22 15:31:22 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+// button shit
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Baseball is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Baseball is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
